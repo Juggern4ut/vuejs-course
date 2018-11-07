0ef0a4b9b26ddc4150b980dca5962514c3f96e07
Lukas Meier <lukas.meier94@gmail.com>
2018-11-07 19:52:13 +0100

    added stuff

--- lesson2/script.js
+++ lesson2/script.js
@@ -1,10 +1,17 @@
 var app = new Vue({
 	el: 'main',
 	data: {
-		counter: 0
+		counter: 0,
+		x: 0,
+		y: 0
 	}, methods:{
-		increase: function(){
-			this.counter++;
+		increase: function(step, event){
+			this.counter += step;
+			console.log(event);
+		},
+		updateCoordinates: function(event){
+			this.x = event.clientX;
+			this.y = event.clientY;
 		}
 	}
 });
